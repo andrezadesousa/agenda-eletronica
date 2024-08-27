110103c0dc168e21d600186e943309d771e456dc
Andreza Sousa <andrezanascimentodesousa@gmail.com>
2024-08-27 14:37:09 -0300

    fix: registration screen completed

--- agenda-app/src/pages/register/index.tsx
+++ agenda-app/src/pages/register/index.tsx
@@ -1,5 +1,33 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
+import Favicon from "../../assets/icon/favicon.svg";
+import RegisterImage from "../../assets/images/register.jpg";
+
+import "./index.css";
+import { Input } from "../../components/input";
 export const Register = () => {
-  return <div>index</div>;
+  return (
+    <div className="register__container">
+      <div className="register__form">
+        <img src={Favicon} alt="logo" />
+        <Input title="Seu email" placeholder="Email" type="email" />
+        <Input title="Sua senha" placeholder="Senha" type="password" />
+        <button className="register__button">Cadastrar</button>
+      </div>
+      <div className="register__content">
+        <div className="register__infos">
+          <h1 className="register__title">Comece um novo agendamento!</h1>
+          <p className="register__text">
+            E aproveite as vantagens de ter uma agenda eletrônica
+          </p>
+          <div className="register__btn">
+            <p>Você já possui uma conta?</p>
+            <Link to="/">Fazer login</Link>
+          </div>
+        </div>
+        <img src={RegisterImage} alt="register" className="register__image" />
+      </div>
+    </div>
+  );
 };
