de9e34258bf22b3309eb225f9ebe5fdefa8f4ba8
Andreza Sousa <andrezanascimentodesousa@gmail.com>
2024-08-28 19:40:02 -0300

    create AuthContext

--- agenda-app/src/routes/routes.jsx
+++ agenda-app/src/routes/routes.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { React, useEffect, useState } from "react";
 import { Route, BrowserRouter, Routes } from "react-router-dom";
 
 import { Login } from "../pages/login";
@@ -7,17 +7,31 @@ import { Home } from "../pages/home";
 import { AddContact } from "../pages/add-contact";
 import { EditContact } from "../pages/edit-contact";
 
+import { AuthContext, AuthProvider } from "../AuthComtext";
+import { Navigate } from "react-router-dom";
+
 const RouterApp = () => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      setUser({ token });
+    }
+  }, []);
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} exact />
-        <Route path="/sign-up" element={<Register />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/criar-contato" element={<AddContact />} />
-        <Route path="/editar-contato" element={<EditContact />} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} exact />
+          <Route path="/sign-up" element={<Register />} />
+          <Route path="/criar-contato" element={<AddContact />} />
+          <Route path="/editar-contato" element={<EditContact />} />
+          <Route path="/home" element={user ? <Home /> : <Navigate to="/" />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 };
 
